3775e70f72e3f1bfcb77a2892116583b95dba800
evenwu <evendesign@gmail.com>
2014-04-02 16:34:50 +0800

    update

--- js/all.js
+++ js/all.js
@@ -45,54 +45,58 @@ var startGame = function()  {
       startGame();
     }else if (initTime == 0){
       clearTimeout(main);
-      $('.showpoint').html(point).parent().bigtext({maxfontsize: 200});
-      if ( point < 1 ) {
-        comment += "智商跟馬英九相同。"
-      } else if ( point < 6 ) {
-        comment += "白狼掏掏，再挑戰一次"
-      } else if ( point < 7 ) {
-        comment += "快抵抗白衫軍攻勢！再玩一次！"
-      } else if ( point < 8 ) {
-        comment += "你的偷懶害立法院失守了"
-      } else if ( point < 9 ) {
-        comment += "請注意務必逐題審查！"
-      } else if ( point < 10 ) {
-        comment += "快守不住了！立法院需要你的支援！"
-      } else if ( point < 11 ) {
-        comment += "Over My Dead Body"
-      } else if ( point < 12 ) {
-        comment += "涂明義救援中！"
-      } else if ( point < 13 ) {
-        comment += "公聽會有辦等於沒辦！"
-      } else if ( point < 14 ) {
-        comment += "臉書被檢舉照片太醜　王炳忠：言語霸凌!"
-      } else if ( point < 15 ) {
-        comment += "「你們不配當中國人」語畢，哄堂大笑"
-      } else if ( point < 16 ) {
-        comment += "馬總統民調只有九趴，你們是不是也是多數在霸凌少數"
-      } else if ( point < 17 ) {
-        comment += "先立法後答題"
-      } else if ( point < 18 ) {
-        comment += "辨識度堪稱糾察隊"
-      } else if ( point < 19 ) {
-        comment += "辨識度堪稱糾察隊"
-      } else if ( point < 20 ) {
-        comment += "辨識度堪稱糾察隊"
-      } else if ( point < 21 ) {
-        comment += "林飛帆表示：陳為廷在我旁邊啦！"
-      } else if ( point < 25 ) {
-        comment += "你的分辨率太高，TVBS、中天都做不了假新聞..."
-      } else {
-        comment += "你證明了 Z < B"
-      }
-      $('.comment').html(comment);
-      $('body').removeClass('status-playing').addClass('status-end');
-      point = 0;
-      comment = "點評：";
-      initTime = 30;
-      $('.timestamp').html(initTime);
+      game.end();
     }
   },1000)
+};
+
+var endGame = function()  {
+  $('.showpoint').html(point).parent().bigtext({maxfontsize: 200});
+  if ( point < 1 ) {
+    comment += "智商跟馬英九相同。"
+  } else if ( point < 6 ) {
+    comment += "白狼掏掏，再挑戰一次"
+  } else if ( point < 7 ) {
+    comment += "快抵抗白衫軍攻勢！再玩一次！"
+  } else if ( point < 8 ) {
+    comment += "你的偷懶害立法院失守了"
+  } else if ( point < 9 ) {
+    comment += "請注意務必逐題審查！"
+  } else if ( point < 10 ) {
+    comment += "快守不住了！立法院需要你的支援！"
+  } else if ( point < 11 ) {
+    comment += "Over My Dead Body"
+  } else if ( point < 12 ) {
+    comment += "涂明義救援中！"
+  } else if ( point < 13 ) {
+    comment += "公聽會有辦等於沒辦！"
+  } else if ( point < 14 ) {
+    comment += "臉書被檢舉照片太醜　王炳忠：言語霸凌!"
+  } else if ( point < 15 ) {
+    comment += "「你們不配當中國人」語畢，哄堂大笑"
+  } else if ( point < 16 ) {
+    comment += "馬總統民調只有九趴，你們是不是也是多數在霸凌少數"
+  } else if ( point < 17 ) {
+    comment += "先立法後答題"
+  } else if ( point < 18 ) {
+    comment += "辨識度堪稱糾察隊"
+  } else if ( point < 19 ) {
+    comment += "辨識度堪稱糾察隊"
+  } else if ( point < 20 ) {
+    comment += "辨識度堪稱糾察隊"
+  } else if ( point < 21 ) {
+    comment += "林飛帆表示：陳為廷在我旁邊啦！"
+  } else if ( point < 25 ) {
+    comment += "你的分辨率太高，TVBS、中天都做不了假新聞..."
+  } else {
+    comment += "你證明了 Z < B"
+  }
+  $('.comment').html(comment);
+  $('body').removeClass('status-playing').addClass('status-end');
+  point = 0;
+  comment = "點評：";
+  initTime = 30;
+  $('.timestamp').html(initTime);
 };
 
 var game = {
@@ -102,6 +106,9 @@ var game = {
   },
   next : function(){
     randomText();
+  },
+  end : function(){
+    endGame();
   }
 };
 
@@ -138,7 +145,7 @@ $yes.on('click',function(){
     clicked = 1;
     game.next();
   }else if (checkNumb == 0 && clicked == 0){
-    initTime = 0
+    game.end();
   }
 })
 
@@ -148,6 +155,6 @@ $no.on('click',function(){
     clicked = 1;
     game.next();
   }else if (checkNumb == 1 && clicked == 0){
-    initTime = 0
+    game.end();
   }
 })
